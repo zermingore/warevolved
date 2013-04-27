f67936e66dac8b7fcf99d99d8b289b566c19e069
Jean-Philippe Quenord <zermingore@gmail.com>
2013-04-27 17:44:19 +0200

    forgot Font class

---
+++ src/resources/Font.cc
@@ -0,0 +1,37 @@
+#include <resources/Font.hh>
+#include <common/constants.hh>
+
+Font::Font()
+{
+}
+
+
+Font::Font(const std::string file_name,
+		   const std::string name,
+		   unsigned int id)
+{
+  _id = id;
+  _fileName = file_name;
+  _name = name;
+  _loaded = false;
+
+//  this->load();
+}
+
+
+Font::~Font()
+{
+}
+
+bool Font::load()
+{
+  _font->loadFromFile(_fileName); // static_cast<std::string>(FONTS_FOLDER) + _fileName
+
+  return true;
+}
+
+
+bool Font::unload()
+{
+  return false;
+}
---
+++ src/resources/Font.hh
@@ -0,0 +1,30 @@
+#ifndef FONT_HH_
+# define FONT_HH_
+
+# include <resources/Resource.hh>
+# include <common/include.hh>
+
+
+/** \brief Font Resource type
+ ** Derived from Resource
+ */
+
+class Font : public Resource
+{
+public:
+  Font();
+  Font(const std::string file_name,
+		const std::string name,
+		unsigned int id);
+
+  ~Font();
+
+  bool load();
+  bool unload();
+
+
+private:
+  sf::Font *_font; ///< actual Font
+};
+
+#endif /* !FONT_HH_ */
