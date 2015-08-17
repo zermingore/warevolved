b25ec25c5e2cd75af8a5a0ef47671631a2796c90
Jean-Philippe Quenord <zermingore@gmail.com>
2015-08-17 19:38:21 +0200

    Graphics should be managed by the GraphicsEngine so that, the Map now has GraphicsProperties

---
+++ src/graphics/MapGraphicsProperties.cc
@@ -0,0 +1,11 @@
+#include <graphics/MapGraphicsProperties.hh>
+
+
+MapGraphicsProperties::MapGraphicsProperties() :
+  _cellWidth (64),
+  _cellHeight (64),
+  _gridThickness (5),
+  _gridOffsetX (0),
+  _gridOffsetY (0)
+{
+}
