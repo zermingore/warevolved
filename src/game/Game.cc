6e219745b3bb7c3fc9aed526e4316a78c20e150f
Jean-Philippe Quenord <zermingore@gmail.com>
2016-10-11 08:25:11 +0200

    [Clean] Game: removed useless includes

--- src/game/Game.cc
+++ src/game/Game.cc
@@ -1,12 +1,9 @@
 #include <game/Game.hh>
 #include <graphics/GraphicsEngine.hh>
-#include <input/EventManager.hh>
 #include <game/applications/Battle.hh>
-#include <input/KeyManager.hh>
 #include <resources/ResourcesManager.hh>
-#include <common/Status.hh>
-#include <common/State.hh>
 #include <input/InputProcessor.hh>
+
 
 void Game::run()
 {
@@ -18,10 +15,7 @@ void Game::run()
 
   // Game loop: processing events, updating display
   // mode: playing
-  // someone else need to process, not the event manager
-  while (GraphicsEngine::windowIsOpen()
-         && input_processor->process())
-//       && Status::currentState()->eventManager()->execute())
+  while (GraphicsEngine::windowIsOpen() && input_processor->process())
   {
     GraphicsEngine::drawScene(battle);
   }
