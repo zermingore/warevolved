c19ca9bdd3d7beded678351148bff228268cc9aa
Jean-Philippe Quenord <zermingore@gmail.com>
2017-03-01 19:10:01 +0100

    Fixed flood fill and non considered targets (will need to find a way to handle any min/max range though)

--- src/game/PathFinding.cc
+++ src/game/PathFinding.cc
@@ -340,29 +340,35 @@ PathFinding::getTargets(std::shared_ptr<Unit> unit, std::shared_ptr<Cell> cell)
 
   std::vector<std::shared_ptr<Cell>> targets_list;
 
-  // implicit cast into integer to handle negative values: unit_x - cell_x
-  int unit_x = cell->x();
-  int unit_y = cell->y();
+  // implicit cast into integer to handle negative values: unit_c - cell_c
+  int unit_c = cell->c();
+  int unit_l = cell->l();
 
   std::stack<std::pair<int, int>> s;
-  s.push(std::pair<int, int>(unit_x, unit_y));
+  s.push(std::pair<int, int> (unit_c, unit_l));
 
   std::vector<std::pair<int, int>> checked; // already visited cells
+
+  // adding the 4 cells adjacent to the attacker
+  s.emplace(unit_c + 1, unit_l    );
+  s.emplace(unit_c    , unit_l + 1);
+  s.emplace(unit_c - 1, unit_l    );
+  s.emplace(unit_c    , unit_l - 1);
 
   while (!s.empty())
   {
     // getting Cell's coordinates (stack of Coordinates)
     auto current_cell = s.top();
-    int x = current_cell.second;
-    int y = current_cell.first;
+    int c = current_cell.first;
+    int l = current_cell.second;
     s.pop();
 
     // check overflow, Manhattan distance and if we already marked the cell
-    size_t distance(std::abs(unit_x - x) + std::abs(unit_y - y));
-    if (   static_cast<size_t> (x) >= _map->nbColumns()
-        || static_cast<size_t> (y) >= _map->nbLines()
+    size_t distance(std::abs(unit_c - c) + std::abs(unit_l - l));
+    if (   static_cast<size_t> (c) >= _map->nbColumns()
+        || static_cast<size_t> (l) >= _map->nbLines()
         || distance < unit->minRange()
-        || distance > _maxLength + unit->maxRange()
+        || distance > unit->maxRange()
         || std::find(checked.begin(), checked.end(), current_cell) != checked.end())
     {
       // skipping invalid cells
@@ -372,19 +378,19 @@ PathFinding::getTargets(std::shared_ptr<Unit> unit, std::shared_ptr<Cell> cell)
     checked.push_back(current_cell);
 
     // updating target list
-    auto c = (*_map)[current_cell.second][current_cell.first];
-    if (c->unit())
+    auto map_cell = (*_map)[c][l];
+    if (map_cell->unit())
     {
-      if (c->unit()->playerId() != Status::player()->id()) {
-        targets_list.push_back(c);
+      if (map_cell->unit()->playerId() != Status::player()->id()) {
+        targets_list.push_back(map_cell);
       }
     }
 
     // continuing with the four adjacent cells of the current one
-    s.emplace(x + 1, y    );
-    s.emplace(x    , y + 1);
-    s.emplace(x - 1, y    );
-    s.emplace(x    , y - 1);
+    s.emplace(c + 1, l    );
+    s.emplace(c    , l + 1);
+    s.emplace(c - 1, l    );
+    s.emplace(c    , l - 1);
   }
 
   return std::make_shared<std::vector<std::shared_ptr<Cell>>> (targets_list);
