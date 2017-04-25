a9f3424247c300f34077f43c84c04ba8d4e230d1
Jean-Philippe Quenord <zermingore@gmail.com>
2017-04-25 19:41:45 +0200

    build: The autogen script builds in a separate folder

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+# configure the projects, and build in a specific directory
+
+BUILD_DIR=build # build destination
 
 autoreconf --force --install
-./configure
+
+rm -rf "$BUILD_DIR"
+mkdir "$BUILD_DIR"
+pushd "$BUILD_DIR"
+
+../configure # TODO find the configure wherever the build folder is
 make
+
+popd # "$BUILD_DIR"
