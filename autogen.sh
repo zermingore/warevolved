921a1bdd682ee0d10fbaf1f1df850262fd76aff0
Jean-Philippe Quenord <zermingore@gmail.com>
2017-05-11 07:45:41 +0200

    build: Added a parameter to prevent build in autogen.sh (quick and dirty)

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,8 @@
 
 # configure the projects, and build in a specific directory
 
+NO_BUILD=$1
+
 BUILD_DIR=build # build destination
 
 autoreconf --force --install --warnings=all
@@ -11,6 +13,10 @@ mkdir -p "$BUILD_DIR"
 pushd "$BUILD_DIR"
 
 ../configure # TODO find the configure wherever the build folder is
-make
+
+
+if [[ "$NO_BUILD" == "" ]]; then
+   make
+fi
 
 popd # "$BUILD_DIR"
