9adda95401c0b3d73dc65e08692178004b880ec6
Jean-Philippe Quenord <zermingore@gmail.com>
2017-06-30 20:28:27 +0200

    Properly initializing Xlib multi-threads support (Fixing the crash with xcb complaining about the multi-threaded client)

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,4 @@
+#include <X11/Xlib.h> // to initialize the Xlib mutli-threads support
 #include <core/Context.hh>
 #include <game/Game.hh>
 #include <debug/Debug.hh>
@@ -6,6 +7,9 @@
 
 int main(int ac, const char **av)
 {
+  // Initialize the Xlib support for concurrent threads.
+  XInitThreads(); /// \todo don't really know why it cannot be in the Context
+
   /// \todo use getopt, support: resolution, graphic engine ({2,3}D, ASCII), ...
   PRINTF(av);
 
