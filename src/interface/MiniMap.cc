f5faf46925f9a5dfaf7d47b22828b9eaeefbaa01
Jean-Philippe Quenord <zermingore@gmail.com>
2017-09-11 19:30:25 +0200

    Cleaned the MiniMap drawing

--- src/interface/MiniMap.cc
+++ src/interface/MiniMap.cc
@@ -36,47 +36,55 @@ void MiniMap::draw()
   const size_t size_column(_frameSize.first / _map->nbColumns());
   const size_t size_line(_frameSize.second  / _map->nbLines());
 
+  // List of players to get their color
+  const auto players { game::Status::battle()->players() };
+
+  using namespace graphics;
+  const Size2 cell_size(static_cast<component> (size_column),
+                        static_cast<component> (size_line));
+
   const auto cells(_map->cells());
   for (auto col(0u); col < _map->nbColumns(); ++col)
   {
     for (auto line(0u); line < _map->nbLines(); ++line)
     {
-      const auto c {cells[col][line]};
+      const auto c { cells[col][line] };
+      const Coords pos { _position.c + size_column * col,
+                         _position.l + size_line   * line };
+
+      // Draw the terrain
       switch (c->terrain())
       {
         default:
-          auto img(resources::ResourcesManager::getImage("forest"));
-          img->setPosition({ _position.c + size_column * col,
-                             _position.l + size_line * line });
-          img->setSize(static_cast<graphics::component> (size_column),
-                       static_cast<graphics::component> (size_line));
+          auto img { resources::ResourcesManager::getImage("forest") };
+          img->setPosition(pos);
+          img->setSize(cell_size);
           img->draw();
           break;
       }
 
-      if (c->unit())
+      // Draw the units
+      const auto unit {c->unit()};
+      if (unit)
       {
-        auto img(resources::ResourcesManager::getImage("minimap_unit"));
-        img->setPosition({_position.c + size_column * col,
-                          _position.l + size_line * line});
-        img->setSize(static_cast<graphics::component> (size_column),
-                     static_cast<graphics::component> (size_line));
+        auto img { resources::ResourcesManager::getImage("minimap_unit") };
+        img->setPosition(pos);
+        img->setSize(cell_size);
 
-
-        // Using the color of the Player owning unit this Unit
-        auto players {game::Status::battle()->players()};
-        img->setColor(players[c->unit()->playerId()]->color());
-
-        if (c->unit()->played())
+        // Fade the color or use the color of the Player owning this Unit
+        if (unit->played())
         {
-          img->setColor(graphics::Color(127, 127, 127, 150));
+          img->setColor(Color(127, 127, 127, 150));
+        }
+        else
+        {
+          img->setColor(players[unit->playerId()]->color());
         }
 
         img->draw();
       }
     }
   }
-
 }
 
 
