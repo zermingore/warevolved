da2b9900c553a67a84ba60f4535d220093df2377
Jean-Philippe Quenord <zermingore@gmail.com>
2017-09-19 20:01:13 +0200

    Considering Terrain as a class in order to handle its Texture and defense

---
+++ src/game/Terrain.hh
@@ -0,0 +1,41 @@
+/**
+ * \file
+ * \date November 25, 2016
+ * \author Zermingore
+ */
+
+#ifndef TERRAIN_HH_
+# define TERRAIN_HH_
+
+# include <memory>
+# include <graphics/graphic_types.hh>
+
+
+
+/**
+ * \class Terrain
+ * \brief Represents a terrain on a Map Cell
+ */
+class Terrain
+{
+public:
+  /**
+   * \brief Deleted default constructor; attributes required
+   */
+  Terrain() = delete;
+
+  /**
+   * \brief Initializes the terrain with the given defense and texture
+   * \param defense How much cover the terrain offers
+   * \param texture Terrain's associated texture
+   */
+  Terrain(int defense, std::shared_ptr<const graphics::Texture> texture);
+
+
+private:
+  int _defense; ///< Defense value
+  std::shared_ptr<const graphics::Texture> _texture; ///< Graphical image
+};
+
+
+#endif /* !TERRAIN_HH_ */
