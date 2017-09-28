8325e95ad25e7c8af01ebb576eedd07f884315de
Jean-Philippe Quenord <zermingore@gmail.com>
2017-09-28 07:54:29 +0200

    InterfaceElement: no longer passing simple types by reference

--- src/interface/InterfaceElement.hh
+++ src/interface/InterfaceElement.hh
@@ -89,7 +89,7 @@ public:
    * \param scale_x scale in x related to cells size.
    * \param scale_y scale in y related to cells size.
    */
-  void setScale(const float& scale_x, const float& scale_y) {
+  void setScale(const float scale_x, const float scale_y) {
     _scale.x = scale_x;
     _scale.y = scale_y;
   }
@@ -98,7 +98,7 @@ public:
    * \brief scale setter.
    * \param scale scale to apply in x and y related to cells size.
    */
-  void setScale(const float& scale) { setScale(scale, scale); }
+  void setScale(const float scale) { setScale(scale, scale); }
 
   /**
    * \brief rotation setter.
@@ -110,7 +110,7 @@ public:
    *
    * \param rotation angle of the rotation.
    */
-  void setRotation(const float& rotation) { _rotation = rotation; }
+  void setRotation(const float rotation) { _rotation = rotation; }
 
 
   /**
@@ -128,7 +128,7 @@ public:
 
 
 protected:
-  std::string _img_name;                    ///< Associated image name
+  std::string _img_name;                     ///< Associated image name
   std::shared_ptr<resources::Sprite> _image; ///< Associated image
 
   graphics::Pos2 _position; ///< Element position (in pixels)
