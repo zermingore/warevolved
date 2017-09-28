a36a69ec322a7e84abd870c2bf8590334ba9aa09
Jean-Philippe Quenord <zermingore@gmail.com>
2017-09-28 18:58:26 +0200

    Improved comments in Unit header

--- src/game/units/Unit.hh
+++ src/game/units/Unit.hh
@@ -25,42 +25,44 @@ class Team;
 class Unit
 {
 public:
-  ///< \brief Default Constructor
+  /**
+   * \brief Initialize the attributes with default values
+   */
   Unit();
 
   /**
    * \brief x coordinate on the map getter
-   * \return x (column) coordinate on the map
+   * \return x (column) coordinate on the Map
    */
   auto c() { return _coords.c; }
 
   /**
    * \brief x unit coordinate on the map getter
-   * \return y (line) coordinate on the map
+   * \return y (line) coordinate on the Map
    */
   auto l() { return _coords.l; }
 
   /**
    * \brief gets unit's position
-   * \return unit's cell coordinates
+   * \return unit's Cell coordinates
    */
   auto coords() { return _coords; }
 
   /**
-   * \brief sets unit's position and _played to true (we moved the unit)
-   * \param coords: cell's coordinates to set unit's position
+   * \brief sets unit's position
+   * \param coords New unit Cell's coordinates
    */
   void setCoords(const Coords coords) { _coords = coords; }
 
   /**
    * \brief _playerId getter
+   * \return Id of the Player owning this Unit
    */
   auto playerId() { return _playerId; }
 
   /**
    * \brief _played getter
-   * \return true if the unit has already been played
-   *   false otherwise
+   * \return true if the unit has already been played, false otherwise
    */
   auto played() { return _played; }
 
@@ -90,22 +92,25 @@ public:
 
   /**
    * \brief _attackValue getter
-   * \return attack value in hit points
+   * \return Attack value in hit points
    */
   auto attackValue() { return _attackValue; }
 
   /**
    * \brief _motionValue getter
+   * \return The number of Cells a Unit can cross in one turn
    */
   auto motionValue() { return _motionValue; }
 
   /**
-   * \brief minimal attack range getter
+   * \brief _minRange getter
+   * \return Minimal attack range (in Cells)
    */
   auto minRange() { return _minRange; }
 
   /**
-   * \brief maximal attack range getter
+   * \brief _maxRange getter
+   * \return Maximal attack range getter (in Cells)
    */
   auto maxRange() { return _maxRange; }
 
