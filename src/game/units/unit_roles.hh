0feb198a8425befcf06d939d56fc84546208d9e2
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2019-08-30 19:03:49 +0200

    [Units] Forgot unit roles enum

---
+++ src/game/units/unit_roles.hh
@@ -0,0 +1,15 @@
+#ifndef UNIT_ROLES_HH_
+# define UNIT_ROLES_HH_
+
+enum class e_unit_role
+{
+    PASSENGER // eventually multiple passengers
+  , DRIVER    // or pilot
+  , COPILOT   // may be necessary (airplane)
+  , LOADER    // ex: tank
+  , GUNNER    // list (airplane)
+  , MECHANIC
+};
+
+
+#endif /* !UNIT_ROLES_HH_ */
