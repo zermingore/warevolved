4ce41208b203790cbefd48ed8c0937a5c86a6335
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2019-09-26 16:10:01 +0200

    [Tests] Hidding expected error messages in smoke tests

--- tests/smoke/test_cases/options.sh
+++ tests/smoke/test_cases/options.sh
@@ -13,7 +13,7 @@ fi
 pushd "$ROOT_PROJECT" > /dev/null
   "$BIN_WE" --help > /dev/null || return 1
   "$BIN_WE" --version > /dev/null || return 1
-  "$BIN_WE" --not-an-option > /dev/null && return 1
-  "$BIN_WE" --load-map=this_map_does_not_exist > /dev/null && return 1;
-  "$BIN_WE" --saves-directory=/not_writable > /dev/null && return 1;
+  "$BIN_WE" --not-an-option > /dev/null 2>&1 && return 1
+  "$BIN_WE" --load-map=this_map_does_not_exist > /dev/null 2>&1 && return 1;
+  "$BIN_WE" --saves-directory=/not_writable > /dev/null 2>&1 && return 1;
 popd > /dev/null
