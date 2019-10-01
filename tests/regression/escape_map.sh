ac5861c3417c5868e29a265dccf5645f0f931742
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2019-10-01 17:39:16 +0200

    [Tests] The escape_map non regression tests really triggers a segfault

--- tests/regression/escape_map.sh
+++ tests/regression/escape_map.sh
@@ -1,7 +1,7 @@
 # Segfault canceling on the menu map
 
 TMP=/tmp/we_replay_test
-echo '1 0' > "$TMP" # escape after 1ms
+echo '1 6' > "$TMP" # escape after 1ms
 
 timeout 2 "${BIN_WE}" -r --replay-file="$TMP" >> /dev/null
 if [[ $? -eq 124 ]]; then
