e96228bf8cd7b461b6652189926de445a69533e7
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2019-10-13 16:56:46 +0200

    [Clean] Fixed indentation in tools/

--- tools/check_headers.sh
+++ tools/check_headers.sh
@@ -42,26 +42,26 @@ COLOR_NORMAL=$(echo -e '\e[0m')
 
 function usage()
 {
-    echo -e "\n\t./$(basename "$0") path_to_project_root"
+  echo -e "\n\t./$(basename "$0") path_to_project_root"
 }
 
 function version()
 {
-    echo -e "$(basename "$0") Version 1.0.0"
+  echo -e "$(basename "$0") Version 1.0.0"
 }
 
 function help()
 {
-    echo
-    echo "Check if the headers are self sufficient in a project"
-    echo "The project must have a main.cc"
-    echo
-    echo "  -h --help    display this help"
-    echo "  -v --version display the program version"
-    echo
-    echo "                              Written for War Evolved"
-    echo
-    usage
+  echo
+  echo "Check if the headers are self sufficient in a project"
+  echo "The project must have a main.cc"
+  echo
+  echo "  -h --help    display this help"
+  echo "  -v --version display the program version"
+  echo
+  echo "                              Written for War Evolved"
+  echo
+  usage
 }
 
 
@@ -71,95 +71,95 @@ function help()
 # Exits if the arguments are invalid
 function check_arguments()
 {
-    # Check for help / version parameter
-    if [[ $1 == "-h" || $1 == "--help" ]]; then
-        help
-        exit 0
-    fi
+  # Check for help / version parameter
+  if [[ $1 == "-h" || $1 == "--help" ]]; then
+    help
+    exit 0
+  fi
 
-    if [[ $1 == "-v" || $1 == "--version" ]]; then
-        version
-        exit 0
-    fi
+  if [[ $1 == "-v" || $1 == "--version" ]]; then
+    version
+    exit 0
+  fi
 
 
-    # The path must be provided
-    if [[ $# -ne 1 ]]; then
-        echo ""
-        echo "Invalid Arguments $*"
-        echo "One argument expected (path to the project root)"
-        usage "$0"
-        exit 1
-    fi
+  # The path must be provided
+  if [[ $# -ne 1 ]]; then
+    echo ""
+    echo "Invalid Arguments $*"
+    echo "One argument expected (path to the project root)"
+    usage "$0"
+    exit 1
+  fi
 
-    # The argument must be a directory
-    if [[ ! -d "$1" ]]; then
-        echo ""
-        echo "Expected the root path of the project"
-        usage "$0"
-        exit 1
-    fi
+  # The argument must be a directory
+  if [[ ! -d "$1" ]]; then
+    echo ""
+    echo "Expected the root path of the project"
+    usage "$0"
+    exit 1
+  fi
 
-    # The path must contain some headers
-    if [[ ! $(find "$1" -name \*.hh) ]]; then
-        echo ""
-        echo "Some header must exits in the given path"
-        usage "$0"
-        exit 1
-    fi
+  # The path must contain some headers
+  if [[ ! $(find "$1" -name \*.hh) ]]; then
+    echo ""
+    echo "Some header must exits in the given path"
+    usage "$0"
+    exit 1
+  fi
 
-    # A main must be at the root
-    if [[ ! -f "${1}/main.cc" ]]; then
-        echo ""
-        echo "The main was not found in the given path"
-        usage "$0"
-        exit 1
-    fi
+  # A main must be at the root
+  if [[ ! -f "${1}/main.cc" ]]; then
+    echo ""
+    echo "The main was not found in the given path"
+    usage "$0"
+    exit 1
+  fi
 }
 
 
 
 function main()
 {
-    local ret_val=0 # Compilations return values accumulator
+  local ret_val=0 # Compilations return values accumulator
 
-    check_arguments "$@"
+  check_arguments "$@"
 
-    tmp_src=/tmp/check_headers
-    test_header="${tmp_src}/test_header.cc"
-    rm -rf "${tmp_src}"
-    mkdir "${tmp_src}"
-    cp -a "$1"/. "${tmp_src}"
+  tmp_src=/tmp/check_headers
+  test_header="${tmp_src}/test_header.cc"
+  rm -rf "${tmp_src}"
+  mkdir "${tmp_src}"
+  cp -a "$1"/. "${tmp_src}"
 
-    CXXINCLUDES="-I$1 -I$1/../lib/" # -I.
-    COMPILE="$CXX $CXXINCLUDES $CXXFLAGS -o ${tmp_src}/unused_header"
-    echo "Checking headers in $1"
-    for header in $(find "$1" -name \*.hh); do
-        echo -n "Checking $header ..."
+  CXXINCLUDES="-I$1 -I$1/../lib/" # -I.
+  COMPILE="$CXX $CXXINCLUDES $CXXFLAGS -o ${tmp_src}/unused_header"
+  echo "Checking headers in $1"
+  for header in $(find "$1" -name \*.hh); do
+    echo -n "Checking $header ..."
 
-        # Compiling the header in stand-alone
-        $COMPILE "$header"
-        ret_val=$(($? + ret_val))
+    # Compiling the header in stand-alone
+    $COMPILE "$header"
+    ret_val=$(($? + ret_val))
 
-        # Compiling a trivial main (with 2 inclusions to check the guards)
-        header_in_src=${header##$1}
-        echo "#include \"$header_in_src\"" > $test_header
-        echo "#include \"$header_in_src\"" >> $test_header
-        echo "int main() { return 0; }" >> $test_header
-        $COMPILE "$test_header"
-        ret_val=$(($? + ret_val))
+    # Compiling a trivial main (with 2 inclusions to check the guards)
+    header_in_src=${header##$1}
+    echo "#include \"$header_in_src\"" > $test_header
+    echo "#include \"$header_in_src\"" >> $test_header
+    echo "int main() { return 0; }" >> $test_header
+    $COMPILE "$test_header"
+    ret_val=$(($? + ret_val))
 
-        echo "$COLOR_GREEN done $COLOR_NORMAL"
-    done
+    echo "$COLOR_GREEN done $COLOR_NORMAL"
+  done
 
-    if [[ $ret_val -ne 0 ]]; then
-        echo
-        echo -e "\t $COLOR_RED NOT EVERY INCLUDE PASSED THE TEST $COLOR_NORMAL"
-        exit 1
-    fi
-
+  if [[ $ret_val -ne 0 ]]; then
     echo
-    echo -e "$COLOR_GREEN Every header complied $COLOR_NORMAL"
+    echo -e "\t $COLOR_RED NOT EVERY INCLUDE PASSED THE TEST $COLOR_NORMAL"
+    exit 1
+  fi
+
+  echo
+  echo -e "$COLOR_GREEN Every header complied $COLOR_NORMAL"
 }
 
 
--- tools/coding_style.sh
+++ tools/coding_style.sh
@@ -16,28 +16,33 @@ COLOR_NORMAL=$(echo -e '\e[0m')
 
 function usage()
 {
-    echo -e "\n\t./$(basename "$0") path_to_project_sources"
+  echo -e "\n\t./$(basename "$0") path_to_project_sources"
 }
+
+
 
 function version()
 {
-    echo -e "$(basename "$0") Version 1.0.0"
+  echo -e "$(basename "$0") Version 1.0.0"
 }
+
+
 
 function help()
 {
-    echo
-    echo "Check the coding style of the project"
-    echo "Currently checks:"
-    echo "- The maximal line length (${MAX_COLUMNS} columns, including '\\n')"
-    echo
-    echo "  -h --help    display this help"
-    echo "  -v --version display the program version"
-    echo
-    echo "                              Written for War Evolved"
-    echo
-    usage
+  echo
+  echo "Check the coding style of the project"
+  echo "Currently checks:"
+  echo "- The maximal line length (${MAX_COLUMNS} columns, including '\\n')"
+  echo
+  echo "  -h --help    display this help"
+  echo "  -v --version display the program version"
+  echo
+  echo "                              Written for War Evolved"
+  echo
+  usage
 }
+
 
 
 # Checks the arguments validity
@@ -45,83 +50,86 @@ function help()
 # Exits if the arguments are invalid
 function check_arguments()
 {
-    # Check for help / version parameter
-    if [[ $1 == "-h" || $1 == "--help" ]]; then
-        help
-        exit 0
-    fi
+  # Check for help / version parameter
+  if [[ $1 == "-h" || $1 == "--help" ]]; then
+    help
+    exit 0
+  fi
 
-    if [[ $1 == "-v" || $1 == "--version" ]]; then
-        version
-        exit 0
-    fi
+  if [[ $1 == "-v" || $1 == "--version" ]]; then
+    version
+    exit 0
+  fi
 
 
-    # The path must be provided
-    if [[ $# -ne 1 ]]; then
-        echo ""
-        echo "Invalid Arguments $*"
-        echo "One argument expected (path to the project sources)"
-        usage "$0"
-        exit 1
-    fi
+  # The path must be provided
+  if [[ $# -ne 1 ]]; then
+    echo ""
+    echo "Invalid Arguments $*"
+    echo "One argument expected (path to the project sources)"
+    usage "$0"
+    exit 1
+  fi
 
-    # The argument must be a directory
-    if [[ ! -d "$1" ]]; then
-        echo ""
-        echo "Expected the root path of the project"
-        usage "$0"
-        exit 1
-    fi
+  # The argument must be a directory
+  if [[ ! -d "$1" ]]; then
+    echo ""
+    echo "Expected the root path of the project"
+    usage "$0"
+    exit 1
+  fi
 }
 
 
 
 function check_lines_length()
 {
-    local lines=$(find "$1" -name \*.hh -o -name \*.cc | xargs grep -H ".\{${MAX_COLUMNS},\}")
-    if [[ $lines != "" ]]; then
-        echo "${COLOR_RED}Too long lines detected:${COLOR_NORMAL}"
+  local lines=$(find "$1" -name \*.hh -o -name \*.cc | xargs grep -H ".\{${MAX_COLUMNS},\}")
+  if [[ $lines != "" ]]; then
+    echo "${COLOR_RED}Too long lines detected:${COLOR_NORMAL}"
 
-        # Printing the too long lines
-        IFS=$'\n'
-        for l in $lines; do
-            echo "${COLOR_YELLOW}>${MAX_COLUMNS} col:${COLOR_NORMAL} $l"
-        done
-        unset IFS
-    else
-        echo "${COLOR_GREEN}Maximal line length respected ($MAX_COLUMNS columns with \\n)${COLOR_NORMAL}"
-    fi
+    # Printing the too long lines
+    IFS=$'\n'
+    for l in $lines; do
+      echo "${COLOR_YELLOW}>${MAX_COLUMNS} col:${COLOR_NORMAL} $l"
+    done
+    unset IFS
+  else
+    echo -n "${COLOR_GREEN}"
+    echo -n "Maximal line length respected ($MAX_COLUMNS columns with \\n)"
+    echo "${COLOR_NORMAL}"
+  fi
 }
 
 
 
 function check_tabs()
 {
-    local lines=$(grep -rn -P '\t' "$1" --exclude=*.in)
-    if [[ $lines != "" ]]; then
-        echo "${COLOR_RED}Tabs detected:${COLOR_NORMAL}"
+  local lines=$(grep -rn -P '\t' "$1" --exclude=*.in)
+  if [[ $lines != "" ]]; then
+    echo "${COLOR_RED}Tabs detected:${COLOR_NORMAL}"
 
-        # Printing faulty lines
-        IFS=$'\n'
-        for l in $lines; do
-            echo "$l"
-        done
-        unset IFS
-    else
-        echo "${COLOR_GREEN}No tab detected${COLOR_NORMAL}"
-    fi
+    # Printing faulty lines
+    IFS=$'\n'
+    for l in $lines; do
+      echo "$l"
+    done
+    unset IFS
+  else
+    echo "${COLOR_GREEN}No tab detected${COLOR_NORMAL}"
+  fi
 }
 
 
 
 function main()
 {
-    check_arguments "$@"
+  check_arguments "$@"
 
-    check_lines_length "$1"
-    check_tabs "$1"
+  check_lines_length "$1"
+  check_tabs "$1"
 }
+
 
 
 main "$@"
