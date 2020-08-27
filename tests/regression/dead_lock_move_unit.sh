7da32c152195c7a35571fa190480735059977945
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-08-27 10:20:59 +0200

    [Tests] Fixed log output file

--- tests/regression/dead_lock_move_unit.sh
+++ tests/regression/dead_lock_move_unit.sh
@@ -4,5 +4,5 @@ echo ${BASH_SOURCE%.*}
 timeout 3 "${BIN_WE}" \
         -r --replay-file=${BASH_SOURCE%.*}.replay \
         --load-map=${BASH_SOURCE%.*}.map \
-        > "/tmp/log_test_$0"
+        > "/tmp/we_log_test"
 return $?
