67caf7686831dc069b3f7278fded164ad8795c42
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2022-10-02 12:40:17 +0200

    [Tests] Unit tests draft (using doctest)
    https://github.com/doctest/doctest

--- tests/unit/main.cc
+++ tests/unit/main.cc
@@ -1,4 +1,57 @@
-int main()
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+#include <tools/options/OptionsParser.hh>
+#include <game/Game.hh>
+
+TEST_CASE("Arguments")
 {
-  return 0;
+  SUBCASE("help")
+  {
+    const char* argv[2] = { "prog_name", "--help" };
+    OptionsParser opt{2, argv};
+    try
+    {
+      opt.parse();
+    }
+    catch (const ArgumentsHelpVersionException&)
+    {
+      // Expecting an "arguments help version exception"
+    }
+  }
+
+  SUBCASE("version")
+  {
+    const char* argv[2] = { "prog_name", "--version" };
+    OptionsParser opt{2, argv};
+    try
+    {
+      opt.parse();
+    }
+    catch (const ArgumentsHelpVersionException&)
+    {
+      // Expecting an "arguments help version exception"
+    }
+  }
+
+  SUBCASE("Invalid argument")
+  {
+    const char* argv[2] = { "prog_name", "--lol" };
+    OptionsParser opt{2, argv};
+    try
+    {
+      opt.parse();
+    }
+    catch (const ArgumentsException&)
+    {
+      // Expecting an "arguments exception"
+    }
+  }
+
+  SUBCASE("No argument")
+  {
+    const char* argv[1] = { "prog_name" };
+    OptionsParser opt{1, argv};
+    opt.parse();
+  }
 }
